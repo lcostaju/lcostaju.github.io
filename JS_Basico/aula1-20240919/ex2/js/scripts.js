49154ffa392dd5063edea5ecbcdcdd8021409fb3
lcostaju <luciocj_eng@outlook.com>
2024-09-19 21:03:31 -0300

    exercicio 2

--- JS_Basico/aula1-20240919/ex2/js/scripts.js
+++ JS_Basico/aula1-20240919/ex2/js/scripts.js
@@ -1,3 +1,19 @@
 //alert
 //confirm
-prompt()
+// idade = prompt("Digite sua idade");
+
+// console.log(typeof(idade));
+
+// idade = "wilton";
+// console.log(typeof(idade));
+
+// idade = true;
+// console.log(typeof(idade));
+nome = prompt("Qual é o seu nome?");
+
+idade = prompt("Qual sua idade?");
+
+if(idade >= 18)
+    alert(`${nome}, você tem idade para tirar CNH`);
+else
+    alert(`${nome}, você não tem idade para tirar CNH`);
