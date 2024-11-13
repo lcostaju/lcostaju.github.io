4d35c4730346af358fae7ec284c22fae5c859a9e
lcostaju <luciocj_eng@outlook.com>
2024-11-13 18:05:49 -0300

    lista06

---
+++ JS_Basico/Lista06/ex02/js/script.js
@@ -0,0 +1,35 @@
+
+
+document.getElementById("cd1").addEventListener("mouseover",function(){
+    document.getElementById("cd1").style.backgroundColor = "green";
+});
+
+document.getElementById("cd2").addEventListener("mouseover",function(){
+    document.getElementById("cd2").style.backgroundColor = "green";
+});
+
+document.getElementById("cd3").addEventListener("mouseover",function(){
+    document.getElementById("cd3").style.backgroundColor = "green";
+});
+
+document.getElementById("cd4").addEventListener("mouseover",function(){
+    document.getElementById("cd4").style.backgroundColor = "green";
+});
+
+
+document.getElementById("cd1").addEventListener("mouseleave",function(){
+    document.getElementById("cd1").style.backgroundColor = "rgb(109, 210, 109)";
+});
+
+document.getElementById("cd2").addEventListener("mouseleave",function(){
+    document.getElementById("cd2").style.backgroundColor = "rgb(109, 210, 109)";
+});
+
+document.getElementById("cd3").addEventListener("mouseleave",function(){
+    document.getElementById("cd3").style.backgroundColor = "rgb(109, 210, 109)";
+});
+
+document.getElementById("cd4").addEventListener("mouseleave",function(){
+    document.getElementById("cd4").style.backgroundColor = "rgb(109, 210, 109)";
+});
+
