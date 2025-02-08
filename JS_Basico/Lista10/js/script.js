a6fde966414ba3c19360cfd369641e2eca216c3d
lcostaju <luciocj_eng@outlook.com>
2025-02-08 20:04:30 -0300

    enhance styling and functionality for Lista10, including responsive design, reset styles, and improved score tracking

--- JS_Basico/Lista10/js/script.js
+++ JS_Basico/Lista10/js/script.js
@@ -3,6 +3,9 @@ window.addEventListener("DOMContentLoaded",function(){
     const txtTempo = document.getElementById("txtTempo");
     const txtNumero = document.getElementById("txtNumero");
     const txtParesSort = document.getElementById("txtParesSort");
+    const txtAcertos = document.getElementById("txtAcertos");
+    const txtErros = document.getElementById("txtErros");
+    const percent = document.getElementById("percent");
 
     const btnIniciar = document.getElementById("btnIniciar");
     const btnParar = document.getElementById("btnParar");
@@ -10,6 +13,16 @@ window.addEventListener("DOMContentLoaded",function(){
 
     var numerosSorteados = 0;
     var numerosPares = 0;
+    var playNums = false;
+
+    function zerarRegistros(){
+        numerosSorteados = 0;
+        numerosPares = 0;
+        txtAcertos.innerHTML = "0";
+        txtErros.innerHTML = "0";
+        txtParesSort.innerHTML = "0";
+        percent.innerHTML = "(0%)";
+    }
 
     function gerarNumeroAleatorio(min,max){
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -24,6 +37,7 @@ window.addEventListener("DOMContentLoaded",function(){
             numerosPares++;
 
         txtParesSort.innerHTML = numerosPares;
+        
     }
 
     function gerarNumero(){
@@ -33,14 +47,23 @@ window.addEventListener("DOMContentLoaded",function(){
             if(dificuldade == "0"){
                 randomNums = setInterval(function(){
                     numAleatorio();
+                    txtNumero.style.color = "black";
+                    txtNumero.style.pointerEvents = "auto";
+                    percent.innerHTML = `(${((parseInt(txtAcertos.innerHTML) / numerosPares)*100).toFixed(2)}%)`;
                 },1000);
             }else if(dificuldade == "1"){
                 randomNums = setInterval(function(){
                     numAleatorio();
+                    txtNumero.style.color = "black";
+                    txtNumero.style.pointerEvents = "auto";
+                    percent.innerHTML = `(${((parseInt(txtAcertos.innerHTML) / numerosPares)*100).toFixed(2)}%)`;
                 },800);
             }else{
                 randomNums = setInterval(function(){
                     numAleatorio();
+                    txtNumero.style.color = "black";
+                    txtNumero.style.pointerEvents = "auto";
+                    percent.innerHTML = `(${((parseInt(txtAcertos.innerHTML) / numerosPares)*100).toFixed(2)}%)`;
                 },700);
             }
         
@@ -64,6 +87,19 @@ window.addEventListener("DOMContentLoaded",function(){
         txtNumero.innerHTML = "_";
     }
 
+    function setTempo(){
+        var nivel = selNivel.value;
+        if(nivel == "0"){
+            txtTempo.innerHTML = "01:45";
+        }else if(nivel == "1"){
+            txtTempo.innerHTML = "01:15";
+        }else if(nivel == "2"){
+            txtTempo.innerHTML = "00:05";
+        }else{
+            txtTempo.innerHTML = "00:00";
+        }
+    }
+
     function iniciarContagem(tempo){
         var tempo = tempo.split(":");
         var minutos = parseInt(tempo[0]);
@@ -72,7 +108,11 @@ window.addEventListener("DOMContentLoaded",function(){
         intervalo = setInterval(function(){
             if(minutos == 0 && segundos == 0){
                 clearInterval(intervalo);
+                clearInterval(randomNums);
+                setTempo();
                 alert("Tempo esgotado");
+                selNivel.disabled = false;
+                txtNumero.innerHTML = "_";
             }else{
                 if(segundos == 0){
                     minutos--;
@@ -92,10 +132,13 @@ window.addEventListener("DOMContentLoaded",function(){
         if(selNivel.value == ""){
             alert("Selecione um nível de dificuldade");
         }else{
+            playNums = true;
             selNivel.disabled = true;
             iniciarContagem(txtTempo.innerHTML);
             gerarNumero();
-            
+            if(txtNumero.innerHTML == "_"){
+                zerarRegistros();
+            }
         }
         
 
@@ -105,23 +148,32 @@ window.addEventListener("DOMContentLoaded",function(){
         pararContagem();
         pararTempo();
         selNivel.disabled = false;
+        playNums = false;
+        selNivel.value = "";
+        zerarRegistros();
     });
 
     btnPausar.addEventListener("click",function(){
         pausarTempo();
         pausarContagem();
+        playNums = false;
     });
 
     selNivel.addEventListener("change",function(){
-        var nivel = selNivel.value;
-        if(nivel == "0"){
-            txtTempo.innerHTML = "01:45";
-        }else if(nivel == "1"){
-            txtTempo.innerHTML = "01:15";
-        }else if(nivel == "2"){
-            txtTempo.innerHTML = "00:30";
-        }else{
-            txtTempo.innerHTML = "00:00";
+        setTempo();
+    });
+
+    txtNumero.addEventListener("click",function(){
+        if(playNums){
+            if(txtNumero.innerHTML % 2 == 0){
+                txtAcertos.innerHTML = parseInt(txtAcertos.innerHTML) + 1;
+                txtNumero.style.color = "green";
+                txtNumero.style.pointerEvents = "none";
+            }else{
+                txtErros.innerHTML = parseInt(txtErros.innerHTML) + 1;
+                txtNumero.style.color = "red";
+            }
+            
         }
     });
 });
