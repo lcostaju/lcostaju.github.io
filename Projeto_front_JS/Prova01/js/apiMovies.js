0c13ca195c884a0e6b55b49df61f0cd28e228fbd
lcostaju <luciocj_eng@outlook.com>
2025-05-04 01:00:12 -0300

    Implement code changes to enhance functionality and improve performance

--- Projeto_front_JS/Prova01/js/apiMovies.js
+++ Projeto_front_JS/Prova01/js/apiMovies.js
@@ -9,13 +9,22 @@ const getMovies = async () => {
         }
       };
       
-      const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+      const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1', options)
       const data = await response.json();
-      //console.log(data.results)
-        // .then(res => res.json())
-        // .then(res => console.log(res))
-        // .catch(err => console.error(err));
         return data;
 }
+const getGenres = async () =>{
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiM2I0NWZmOTMxNjUwZDRiNmU0NTY3ODY5OWM0MGQ0OSIsIm5iZiI6MTc0NjA1Mjc4Ny4zNjgsInN1YiI6IjY4MTJhNmIzNDg1Yjc2NmVkZGE0OTAwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GQ-DtlWH7og8YO5U0GeQjBeXknz3YEkk7jSDwtAzclE'
+    }
+  };
 
-export default getMovies;
+  const response = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=pt', options);
+  const data = await response.json();
+  return data;
+}
+
+export {getMovies,getGenres};
--- Projeto_front_JS/Prova01/js/main.js
+++ Projeto_front_JS/Prova01/js/main.js
@@ -1,10 +1,73 @@
-import getMovies from "./apiMovies.js";
+import { getMovies, getGenres } from "./apiMovies.js";
 
-window.addEventListener("DOMContentLoaded",()=>{
-    showMovies();
+window.addEventListener("DOMContentLoaded", async () => {
+    const cardContainer = document.getElementById("cardContainer");
+
+     // Aguarda a resolução da função showMovies
+     const movies = await showMovies();
+     console.log(movies);
+ 
+     // Aqui você pode usar os dados para renderizar os elementos na página
+     movies.forEach(movie => {
+        const card = document.createElement("div");
+        //Insere imagem
+         const img = document.createElement("img");
+         img.src = movie.poster;
+         img.alt = movie.title;
+         card.appendChild(img);
+         //Insere Titulo
+         const title = document.createElement("p");
+         title.innerHTML = movie.title;
+         card.appendChild(title);
+         //Insere Generos
+         const genres = document.createElement("p");
+         genres.innerHTML = `Genêros: ${movie.genres.join(", ")}`;
+         card.appendChild(genres);
+         //Insere data de lançamento
+         const releaseDate = document.createElement("p");
+         releaseDate.innerHTML = `Data de lançamento: ${movie.date}`;
+         card.appendChild(releaseDate);
+         //Insere Avaliação
+         const rating = document.createElement("p");
+         rating.innerHTML = `Avaliação: ${movie.avaliacao}`;
+         card.appendChild(rating);
+        
+         cardContainer.appendChild(card);
+
+     });
 });
 
-const showMovies = async ()=>{
-    const dataMoveis = await getMovies();
-    console.log(dataMoveis);
+const showMovies = async () => {
+    const dataMovies = await getMovies();
+    const dataGenres = await getGenres();
+
+    let moviesInfo = [];
+
+    dataMovies.results.forEach(element => {
+        let auxObj = {};
+
+        auxObj.title = element.title;
+        auxObj.poster = `https://image.tmdb.org/t/p/w200${element.poster_path}`;
+        auxObj.genres = element.genre_ids.map(id => {
+            const genre = dataGenres.genres.find(g => g.id === id);
+            return genre ? genre.name : null;
+        });
+        auxObj.overview = element.overview;
+        auxObj.avaliacao = element.vote_average;
+        auxObj.date = formatarData(element.release_date);
+
+        moviesInfo.push(auxObj);
+
+    });
+
+    //console.log(moviesInfo.slice(0, 10));
+    return moviesInfo.slice(0,10);
 }
+
+const formatarData = (dataISO) => {
+    const data = new Date(dataISO);
+    const opcoes = { day: 'numeric', month: 'short', year: 'numeric' };
+    return new Intl.DateTimeFormat('pt-BR', opcoes).format(data);
+};
+
+
