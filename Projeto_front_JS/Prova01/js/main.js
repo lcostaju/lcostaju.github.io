2fa6b11f6765387eb0ae147ddcde1b6135e9ee53
lcostaju <luciocj_eng@outlook.com>
2025-05-04 03:24:11 -0300

    refactor: clean up showMovies function by removing commented code and improving readability

--- Projeto_front_JS/Prova01/js/main.js
+++ Projeto_front_JS/Prova01/js/main.js
@@ -1,42 +1,7 @@
 import { getMovies, getGenres } from "./apiMovies.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-    
     showMovies();
-     // Aguarda a resolução da função showMovies
-    //  const movies = await showMovies();
-    //  console.log(movies);
- 
-     // Aqui você pode usar os dados para renderizar os elementos na página
-    //  movies.forEach(movie => {
-    //     const card = document.createElement("div");
-    //     //Insere Titulo
-    //     const title = document.createElement("p");
-    //     title.innerHTML = movie.title;
-    //     card.appendChild(title);
-    //     //Insere imagem
-    //      const img = document.createElement("img");
-    //      img.src = movie.poster;
-    //      img.alt = movie.title;
-    //      card.appendChild(img);
-         
-    //      //Insere Generos
-    //      const genres = document.createElement("p");
-    //      genres.innerHTML = `Genêros: ${movie.genres.join(", ")}`;
-    //      card.appendChild(genres);
-    //      //Insere data de lançamento
-    //      const releaseDate = document.createElement("p");
-    //      releaseDate.innerHTML = `Data de lançamento: ${movie.date}`;
-    //      card.appendChild(releaseDate);
-    //      //Insere Avaliação
-    //      const rating = document.createElement("p");
-    //      rating.innerHTML = `Avaliação: ${movie.avaliacao.toFixed(2)}/10`;
-    //      card.appendChild(rating);
-    //      card.className = "movieCard";
-        
-    //      cardContainer.appendChild(card);
-
-    //  });
 });
 
 const showMovies = async () => {
@@ -58,13 +23,13 @@ const showMovies = async () => {
         auxObj.overview = element.overview;
         auxObj.avaliacao = element.vote_average;
         auxObj.date = formatarData(element.release_date);
+        auxObj.id = element.id;
 
         moviesInfo.push(auxObj);
 
     });
 
-    //console.log(moviesInfo.slice(0, 10));
-    let movies = moviesInfo.slice(0,10);
+    let movies = moviesInfo.slice(0, 10);
 
     // Aqui você pode usar os dados para renderizar os elementos na página
     movies.forEach(movie => {
@@ -74,28 +39,28 @@ const showMovies = async () => {
         title.innerHTML = movie.title;
         card.appendChild(title);
         //Insere imagem
-         const img = document.createElement("img");
-         img.src = movie.poster;
-         img.alt = movie.title;
-         card.appendChild(img);
-         
-         //Insere Generos
-         const genres = document.createElement("p");
-         genres.innerHTML = `Genêros: ${movie.genres.join(", ")}`;
-         card.appendChild(genres);
-         //Insere data de lançamento
-         const releaseDate = document.createElement("p");
-         releaseDate.innerHTML = `Data de lançamento: ${movie.date}`;
-         card.appendChild(releaseDate);
-         //Insere Avaliação
-         const rating = document.createElement("p");
-         rating.innerHTML = `Avaliação: ${movie.avaliacao.toFixed(2)}/10`;
-         card.appendChild(rating);
-         card.className = "movieCard";
-        
-         cardContainer.appendChild(card);
+        const img = document.createElement("img");
+        img.src = movie.poster;
+        img.alt = movie.title;
+        card.appendChild(img);
 
-     });
+        //Insere Generos
+        const genres = document.createElement("p");
+        genres.innerHTML = `Genêros: ${movie.genres.join(", ")}`;
+        card.appendChild(genres);
+        //Insere data de lançamento
+        const releaseDate = document.createElement("p");
+        releaseDate.innerHTML = `Data de lançamento: ${movie.date}`;
+        card.appendChild(releaseDate);
+        //Insere Avaliação
+        const rating = document.createElement("p");
+        rating.innerHTML = `Avaliação: ${movie.avaliacao.toFixed(2)}/10`;
+        card.appendChild(rating);
+        card.className = "movieCard";
+
+        cardContainer.appendChild(card);
+
+    });
 }
 
 const formatarData = (dataISO) => {
