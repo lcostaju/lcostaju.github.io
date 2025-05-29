78b56b1bd56ed2171f7ddf999d611611c2accc20
lcostaju <luciocj_eng@outlook.com>
2025-05-29 19:33:54 -0300

    feat: initialize React app with Vite setup
    - Created package.json with dependencies and scripts for development
    - Added main application component (App.jsx) and a form component (Formulario1.jsx)
    - Configured entry point in main.jsx and set up Vite configuration
    - Included ESLint configuration for code quality
    - Added basic styling in App.css and index.css
    - Created index.html for the application structure
    - Included React and Vite logos for demonstration
    - Added .gitignore to exclude unnecessary files
    - Provided README.md with project information and setup instructions

---
+++ Projeto_front_JS/aula20250529/app03/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Formulario1 from './components/Formulario1';
+
+function App() {
+  return (
+    <>
+    <Formulario1/>
+    </>
+  );
+}
+
+export default App;
---
+++ Projeto_front_JS/aula20250529/app03/src/components/Formulario1.jsx
@@ -0,0 +1,16 @@
+
+
+function Formulario1(){
+    return(
+        <>
+        <h2>Formulário 1</h2>
+        <form action="">
+            <label htmlFor="">Nome:</label>
+            <input type="text"/>
+        </form>
+        </>
+        
+    );
+}
+
+export default Formulario1;
